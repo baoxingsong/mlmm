5e2df0e2604c27219132334246262a915abf54be
Baoxing Song <songbaoxing168@163.com>
2018-03-06 00:20:10 +0100

    test under linux

--- src/service/emmax_service.cpp
+++ src/service/emmax_service.cpp
@@ -24,6 +24,8 @@
 ************************************************************************/
 
 #include "emmax_service.h"
+
+
 void emma_test ( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & genotype, const double & man_l, const double & man_u ){
 
     My_matrix<double> x(genotype.get_number_of_individual(), 1);
@@ -78,11 +80,9 @@ void emma_test ( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & geno
     }
 }
 
-
-void emmax_test( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & genotype, const double & man_l, const double & man_u ){
-
+Emma_result estimate( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & genotype ){
     My_matrix<double> x(genotype.get_number_of_individual(), 1);
-    int i, j, j_1;
+    int i;
     for( i=0; i<genotype.get_number_of_individual(); ++i ){
         x.get_matrix()[i][0]=1;
     }
@@ -94,10 +94,16 @@ void emmax_test( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & geno
     int maxiter = 100;
     Eigen_result eigen_L = _get_eigen_L_( k_i.getKin());
     My_matrix<double> x0(genotype.get_number_of_individual(), 0);
-    Emma_result e_er = get_estimates ( pi.getPhenotypes(), k_i.getKin(), x, x0, eigen_L, ngrids, llim, ulim, eps, method, maxiter); // do some thing here and get several parameters
+    return get_estimates ( pi.getPhenotypes(), k_i.getKin(), x, x0, eigen_L, ngrids, llim, ulim, eps, method, maxiter); // do some thing here and get several parameters
 
+}
+
+void emmax_test( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & genotype, const double & man_l, const double & man_u ){
+    int i, j, j_1;
+    Emma_result e_er = estimate( pi, k_i, genotype );
+    int covariantsNumber = 1;
     int n = genotype.get_number_of_individual();
-    int q = x.get_num_column()+1; //test each genotypic variants one by one
+    int q = covariantsNumber + 1; //test each genotypic variants one by one
     int p = n - q;
 
     // claim those variables firstly could save RAM operation and save computational time begin
@@ -167,21 +173,9 @@ void emmax_test( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & geno
 
 void emmax_test_multi_allic_single_test(phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & genotype, const double & man_l, const double & man_u ){
 
-    My_matrix<double> x(genotype.get_number_of_individual(), 1);
     int i, j, j_1;
-    for( i=0; i<genotype.get_number_of_individual(); ++i ){
-        x.get_matrix()[i][0]=1;
-    }
-    int ngrids = 100;
-    double llim = -10.0;
-    double ulim = 10.0;
-    double eps = 0.00000001;
-    std::string method="REML";
-    int maxiter = 100;
-    Eigen_result eigen_L = _get_eigen_L_( k_i.getKin());
-    My_matrix<double> x0(genotype.get_number_of_individual(), 0);
-    Emma_result e_er = get_estimates ( pi.getPhenotypes(), k_i.getKin(), x, x0, eigen_L, ngrids, llim, ulim, eps, method, maxiter); // do some thing here and get several parameters
-
+    Emma_result e_er = estimate( pi, k_i, genotype );
+    int covariantsNumber = 1;
     int n = genotype.get_number_of_individual();
 
     // claim those variables firstly could save RAM operation and save computational time begin
@@ -210,14 +204,14 @@ void emmax_test_multi_allic_single_test(phenotype_impl & pi, Kinship_matrix_impl
 
             std::vector<int> allThisStates_vector;
             for (std::map<int, int>::iterator ittt = allThisStates.begin(); ittt != allThisStates.end(); ++ittt) {
-                if( ittt->second > man_l && ittt->second < man_u ){
+                if( ittt->second >= man_l && ittt->second <= man_u && ittt->second!=missing_genotype){
                     allThisStates_vector.push_back(ittt->first);
                 }
             }
-            if( allThisStates_vector.size() > 0 ){
+            if( allThisStates_vector.size() > 1 ){
 
                 size = allThisStates_vector.size();
-                q = x.get_num_column() + size - 1;
+                q = covariantsNumber + size - 1;
                 p = n - q;
 
                 My_matrix<double> full_x(n, q);
@@ -225,7 +219,7 @@ void emmax_test_multi_allic_single_test(phenotype_impl & pi, Kinship_matrix_impl
                 for (i = 0; i < n; ++i) {
                     full_x.get_matrix()[i][0] = 1;
                 }
-                for (i = 0; i < n; ++i) { // here we take missing value as a state
+                for (i = 0; i < n; ++i) {
                     for (j_1 = 0; j_1 < (size - 1); ++j_1) { // left one allele
                         if (allThisStates_vector[j_1] == genotype.get_genotype_matrix().get_matrix()[i][j]) {
                             this_this_state = 1;
@@ -249,7 +243,7 @@ void emmax_test_multi_allic_single_test(phenotype_impl & pi, Kinship_matrix_impl
                 for (i = 0; i < n; ++i) {
                     mahalanobis_rss += pow(e_er.getY_t().get_array()[i] - x_beta.get_array()[i], 2);
                 }
-                freedome1 = q - x.get_num_column();
+                freedome1 = q - covariantsNumber;
                 f_stat = (h0_rss / mahalanobis_rss - 1) * p / (freedome1);
                 p_val = sf(f_stat, freedome1, p);
                 std::cout << genotype.get_variant_Vector()[j].getChromosome() << "\t"
@@ -260,27 +254,76 @@ void emmax_test_multi_allic_single_test(phenotype_impl & pi, Kinship_matrix_impl
                 }
                 printf("\t%10.20f\n", p_val);
                 //printf("%s\t%d\t%s\t%10.20f\n", genotype.get_variant_Vector()[j].getChromosome(), genotype.get_variant_Vector()[j].getPosition(), genotype.get_variant_Vector()[j].getId(),  p_val);
+            }else if ( allThisStates_vector.size() == 1 && allThisStates_vector[0]!=missing_genotype ){
+                size = 1;
+                q = covariantsNumber+ size;
+                p = n - q;
+
+                My_matrix<double> full_x(n, q);
+                My_matrix<double> X_t(n, q);
+                for (i = 0; i < n; ++i) {
+                    full_x.get_matrix()[i][0] = 1;
+                }
+                for (i = 0; i < n; ++i) { // here we take missing value as a state
+                    if (allThisStates_vector[0] == genotype.get_genotype_matrix().get_matrix()[i][j]) {
+                        this_this_state = 1;
+                    } else {
+                        this_this_state = 0;
+                    }
+                    full_x.get_matrix()[i][1] = this_this_state;
+                }
+                My_Vector<double> beta_est(q);
+                trmul(e_er.getH_sqrt_inv(), full_x, X_t);
+                lsq(X_t, e_er.getY_t(), beta_est);
+
+                for (i = 0; i < n; ++i) {
+                    x_beta.get_array()[i] = 0;
+                    for (j_1 = 0; j_1 < q; ++j_1) {
+                        x_beta.get_array()[i] += X_t.get_matrix()[i][j_1] * beta_est.get_array()[j_1];
+                    }
+                }
+                mahalanobis_rss = 0.0;
+                for (i = 0; i < n; ++i) {
+                    mahalanobis_rss += pow(e_er.getY_t().get_array()[i] - x_beta.get_array()[i], 2);
+                }
+                freedome1 = 1;
+                f_stat = (h0_rss / mahalanobis_rss - 1) * p / (freedome1);
+                p_val = sf(f_stat, freedome1, p);
+                std::cout << genotype.get_variant_Vector()[j].getChromosome() << "\t"
+                          << genotype.get_variant_Vector()[j].getPosition() << "\t"
+                          << genotype.get_variant_Vector()[j].getId();
+                if (p_val == 0.0) {
+                    p_val = 0.00000000000000000001;
+                }
+                printf("\t%10.20f\n", p_val);
             }
         }
     }
 }
 
+
+void summaryLocus( Genotype & genotype, const int & j, std::map<int, int> & allThisStates,
+                   std::vector<int> & allThisStates_vector, const int & n, const double & man_l, const double & man_u){
+    for (int i = 0; i < n; ++i) {
+        if( allThisStates.find(genotype.get_genotype_matrix().get_matrix()[i][j]) == allThisStates.end() ){
+            allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] = 1;
+        }else{
+            allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] = allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] + 1;
+        }
+    }
+    for (std::map<int, int>::iterator ittt = allThisStates.begin(); ittt != allThisStates.end(); ++ittt) {
+        if( ittt->second >= man_l && ittt->second <= man_u && ittt->first!=missing_genotype){
+            allThisStates_vector.push_back(ittt->first);
+        }
+    }
+}
+
+
 void emmax_test_multi_allic_multi_test_null_model( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & genotype, const double & man_l, const double & man_u ){
 
-    My_matrix<double> x(genotype.get_number_of_individual(), 1);
     int i, j, j_1, j_2;
-    for( i=0; i<genotype.get_number_of_individual(); ++i ){
-        x.get_matrix()[i][0]=1;
-    }
-    int ngrids = 100;
-    double llim = -10.0;
-    double ulim = 10.0;
-    double eps = 0.00000001;
-    std::string method="REML";
-    int maxiter = 100;
-    Eigen_result eigen_L = _get_eigen_L_( k_i.getKin());
-    My_matrix<double> x0(genotype.get_number_of_individual(), 0);
-    Emma_result e_er = get_estimates ( pi.getPhenotypes(), k_i.getKin(), x, x0, eigen_L, ngrids, llim, ulim, eps, method, maxiter); // do some thing here and get several parameters
+    Emma_result e_er = estimate( pi, k_i, genotype );
+    int covariantsNumber = 1;
 
     // claim those variables firstly could save RAM operation and save computational time begin
     int n = genotype.get_number_of_individual();
@@ -305,22 +348,14 @@ void emmax_test_multi_allic_multi_test_null_model( phenotype_impl & pi, Kinship_
 
         }else {
             std::map<int, int> allThisStates;
-            for (i = 0; i < n; ++i) {
-                if( allThisStates.find(genotype.get_genotype_matrix().get_matrix()[i][j]) == allThisStates.end() ){
-                    allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] = 1;
-                }else{
-                    allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] = allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] + 1;
-                }
-            }
-
             std::vector<int> allThisStates_vector;
-            for (std::map<int, int>::iterator ittt = allThisStates.begin(); ittt != allThisStates.end(); ++ittt) {
-                if( ittt->second > man_l && ittt->second < man_u ){
-                    allThisStates_vector.push_back(ittt->first);
-                }
-            }
+            summaryLocus( genotype, j, allThisStates, allThisStates_vector, n, man_l, man_u);
+
             if( allThisStates_vector.size() > 0 ){
                 size = allThisStates_vector.size();
+                if( allThisStates_vector.size() == allThisStates.size() && allThisStates_vector.size()==2 ){ // there are two and only two states, it is enough to test only one of them
+                    --size;
+                }
                 for (j_1 = 0; j_1 < size; ++j_1) {
                     if (allThisStates_vector[j_1] != missing_genotype) {
                         for (i = 0; i < n; ++i) {
@@ -363,20 +398,9 @@ void emmax_test_multi_allic_multi_test_null_model( phenotype_impl & pi, Kinship_
 
 void emmax_test_multi_allic_multi_test_full_model( phenotype_impl & pi, Kinship_matrix_impl & k_i, Genotype & genotype, const double & man_l, const double & man_u ){
 
-    My_matrix<double> x(genotype.get_number_of_individual(), 1);
     int i, j, j_1, j_2;
-    for( i=0; i<genotype.get_number_of_individual(); ++i ){
-        x.get_matrix()[i][0]=1;
-    }
-    int ngrids = 100;
-    double llim = -10.0;
-    double ulim = 10.0;
-    double eps = 0.00000001;
-    std::string method="REML";
-    int maxiter = 100;
-    Eigen_result eigen_L = _get_eigen_L_( k_i.getKin());
-    My_matrix<double> x0(genotype.get_number_of_individual(), 0);
-    Emma_result e_er = get_estimates ( pi.getPhenotypes(), k_i.getKin(), x, x0, eigen_L, ngrids, llim, ulim, eps, method, maxiter); // do some thing here and get several parameters
+    Emma_result e_er = estimate( pi, k_i, genotype );
+    int covariantsNumber = 1;
 
     // claim those variables firstly could save RAM operation and save computational time begin
     int n = genotype.get_number_of_individual();
@@ -391,40 +415,27 @@ void emmax_test_multi_allic_multi_test_full_model( phenotype_impl & pi, Kinship_
 
         }else{
             std::map<int, int> allThisStates;
-            for (i = 0; i < n; ++i) {
-                if (allThisStates.find(genotype.get_genotype_matrix().get_matrix()[i][j]) == allThisStates.end()) {
-                    allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] = 1;
-                } else {
-                    allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] =
-                            allThisStates[genotype.get_genotype_matrix().get_matrix()[i][j]] + 1;
-                }
-            }
-
             std::vector<int> allThisStates_vector;
-            for (std::map<int, int>::iterator ittt = allThisStates.begin(); ittt != allThisStates.end(); ++ittt) {
-                if (ittt->second > man_l && ittt->second < man_u) {
-                    allThisStates_vector.push_back(ittt->first);
-                }
-            }
-            if (allThisStates_vector.size() > 0) {
+            summaryLocus( genotype, j, allThisStates, allThisStates_vector, n, man_l, man_u);
 
-                q = allThisStates_vector.size();
+            if (allThisStates_vector.size() > 1) {
+
+                q = covariantsNumber + allThisStates_vector.size() - 1;
                 p = n - q;
                 My_matrix<double> full_X_t(n, q); // there is and intercept, the last state is not included in the full model
                 My_matrix<double> full_x(n, q);
                 for (i = 0; i < n; ++i) {
                     full_x.get_matrix()[i][0] = 1;
                 }
-                for (j_1 = 0; j_1 < (q - 1); ++j_1) {
+                for (j_1 = 0; j_1 < (allThisStates_vector.size() - 1); ++j_1) {
                     for (i = 0; i < n; ++i) {
                         if (allThisStates_vector[j_1] == genotype.get_genotype_matrix().get_matrix()[i][j]) {
-                            full_x.get_matrix()[i][j_1 + 1] = 1;
+                            full_x.get_matrix()[i][j_1 + covariantsNumber] = 1;
                         } else {
-                            full_x.get_matrix()[i][j_1 + 1] = 0;
+                            full_x.get_matrix()[i][j_1 + covariantsNumber] = 0;
                         }
                     }
                 }
-
                 My_Vector<double> full_beta_est(q);
                 trmul(e_er.getH_sqrt_inv(), full_x, full_X_t);
                 lsq(full_X_t, e_er.getY_t(), full_beta_est);
@@ -438,16 +449,16 @@ void emmax_test_multi_allic_multi_test_full_model( phenotype_impl & pi, Kinship_
                 for (i = 0; i < n; ++i) {
                     mahalanobis_rss += pow(e_er.getY_t().get_array()[i] - full_x_beta.get_array()[i], 2);
                 } // this is the full model
+
                 My_Vector<double> beta_est(q - 1);
                 My_matrix<double> X_t(n, q - 1);
                 My_matrix<double> x_1(n, q - 1);
                 for (i = 0; i < n; ++i) {
                     x_1.get_matrix()[i][0] = 1;
                 }
-
-                for (j_1 = 0; j_1 < (q - 1); ++j_1) {
-                    x_index = 1;
-                    for (j_2 = 0; j_2 < (q - 1); ++j_2) {
+                for (j_1 = 0; j_1 < (allThisStates_vector.size() - 1); ++j_1) {
+                    x_index = covariantsNumber;
+                    for (j_2 = 0; j_2 < (allThisStates_vector.size() - 1); ++j_2) {
                         if (j_1 != j_2) { // exclude j_1 each time
                             for (i = 0; i < n; ++i) {
                                 if (allThisStates_vector[j_2] == genotype.get_genotype_matrix().get_matrix()[i][j]) {
@@ -481,6 +492,47 @@ void emmax_test_multi_allic_multi_test_full_model( phenotype_impl & pi, Kinship_
                     }
                     printf("\t%10.20f\n", p_val);
                 }
+            }else if ( allThisStates_vector.size() == 1 ){
+                q = covariantsNumber + 1;
+                p = n - q;
+                My_matrix<double> full_X_t(n, q);
+                My_matrix<double> full_x(n, q);
+                for (i = 0; i < n; ++i) {
+                    full_x.get_matrix()[i][0] = 1;
+                }
+
+                for (i = 0; i < n; ++i) {
+                    if (allThisStates_vector[0] == genotype.get_genotype_matrix().get_matrix()[i][j]) {
+                        full_x.get_matrix()[i][1] = 0;
+                    } else {
+                        full_x.get_matrix()[i][1] = 1;
+                    }
+                }
+                My_Vector<double> full_beta_est(q);
+                trmul(e_er.getH_sqrt_inv(), full_x, full_X_t);
+                lsq(full_X_t, e_er.getY_t(), full_beta_est);
+                for (i = 0; i < n; ++i) {
+                    full_x_beta.get_array()[i] = 0;
+                    for (j_1 = 0; j_1 < q; ++j_1) {
+                        full_x_beta.get_array()[i] += full_X_t.get_matrix()[i][j_1] * full_beta_est.get_array()[j_1];
+                    }
+                }
+                mahalanobis_rss = 0;
+                for (i = 0; i < n; ++i) {
+                    mahalanobis_rss += pow(e_er.getY_t().get_array()[i] - full_x_beta.get_array()[i], 2);
+                }
+
+                h0_rss = e_er.getMahalanobis_rss();
+
+                f_stat = (h0_rss / mahalanobis_rss - 1) * p / freedome1;
+                p_val = sf(f_stat, freedome1, p);
+                std::cout << genotype.get_variant_Vector()[j].getChromosome() << "\t"
+                          << genotype.get_variant_Vector()[j].getPosition() << "\t"
+                          << genotype.get_variant_Vector()[j].getId();
+                if (p_val == 0.0) {
+                    p_val = 0.00000000000000000001;
+                }
+                printf("\t%10.20f\n", p_val);
             }
         }
     }
