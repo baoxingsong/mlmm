2e4e6694a0c23e2ea8a32ccade5b20644ae63e13
Baoxing Song <songbaoxing168@163.com>
2018-03-05 13:35:26 +0100

    test under linux

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "./googletest/googletest/include/gtest/gtest.h"
+//#include "./googletest/googletest/include/gtest/gtest.h"
 
 #include "./src/Control.h"
 
 int main(int argc, char ** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
+//    testing::InitGoogleTest(&argc, argv);
+//    RUN_ALL_TESTS();
     if( argc<=1 ){
         usage();
         return 1;
